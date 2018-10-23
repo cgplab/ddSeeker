3934556c87e904a9ae26622c3e01a300aa991d90
Dario Romagnoli <dario.romagnoli87@gmail.com>
2018-10-23 13:54:50 +0200

    Remove piping

--- ddSeeker_dropSeq_tools.sh
+++ ddSeeker_dropSeq_tools.sh
@@ -50,8 +50,7 @@ Perform tagging, filtering and alignment
 -k <ddSeeker_path>  : Full path of ddSeeker.  Default: 'ddSeeker.py'
 -n <num-cores>      : Number of CPUs unit for parallel computation.  Default: 1.
 -t <tmpdir>         : Where to write temporary files.  Default: a new subdirectory in /tmp
--e                  : Echo commands instead of executing them.  Cannot use with -p.
--p                  : Reduce file I/O by pipeline commands together.  Requires more memory and processing power.
+-e                  : Echo commands instead of executing them.
 -h                  : Print usage and exit.
 EOF
 }
@@ -94,10 +93,6 @@ while getopts ":d:o:g:r:s:k:n:t:eph" options; do
   esac
 done
 shift $(($OPTIND - 1))
-
-if [[ "$pipeline" == 1 && -n "$echo_prefix" ]]; then
-    error_exit "-p and -e cannot be used together"
-fi
 
 check_set "$dropseq_root" "Drop-seq root" "-d"
 check_set "$genomedir" "Genome directory" "-g"
@@ -159,42 +154,23 @@ merge_bam="java -Xmx4000m -jar ${picard_jar} MergeBamAlignment
 tag_with_gene_exon="${dropseq_root}/TagReadWithGeneExon
     O=${outdir}/star_gene_exon_tagged.bam ANNOTATIONS_FILE=${refflat} TAG=GE"
 
-if (( $pipeline == 1 )); then
-    # Stage 1
-    echo stage1
-    $tag_reads -o - | \
-      $qsort_tagged I=/dev/stdin O=/dev/stdout | \
-        $filter_bam I=/dev/stdin O=${unaligned_tagged_filtered_bam}
+# Stage 1
+$echo_prefix $tag_reads -o $unaligned_tagged_bam
+$echo_prefix $qsort_tagged I=$unaligned_tagged_bam O=$unaligned_tagged_qsorted_bam
+$echo_prefix $filter_bam I=$unaligned_tagged_qsorted_bam O=$unaligned_tagged_filtered_bam
+files_to_delete="$files_to_delete $unaligned_tagged_bam"
 
-    exit
-    # Stage 2
-    $sam_to_fastq FASTQ=/dev/stdout | $star_align --readFilesIn /dev/stdin
+# Stage 2
+$echo_prefix $sam_to_fastq FASTQ=$unaligned_tagged_filtered_fastq
+$echo_prefix $star_align --readFilesIn $unaligned_tagged_filtered_fastq
+files_to_delete="$files_to_delete $unaligned_tagged_filtered_fastq"
 
-    # Stage 3
-    $qsort_aligned
+# Stage 3
+$echo_prefix $qsort_aligned
 
-    # Stage 4
-    $merge_bam O=/dev/stdout COMPRESSION_LEVEL=0 | $tag_with_gene_exon I=/dev/stdin
-else
-    # Stage 1
-    $echo_prefix $tag_reads -o $unaligned_tagged_bam
-    $echo_prefix $qsort_tagged I=$unaligned_tagged_bam O=$unaligned_tagged_qsorted_bam
-    $echo_prefix $filter_bam I=$unaligned_tagged_qsorted_bam O=$unaligned_tagged_filtered_bam
-    files_to_delete="$files_to_delete $unaligned_tagged_bam"
-
-    # Stage 2
-    $echo_prefix $sam_to_fastq FASTQ=$unaligned_tagged_filtered_fastq
-    $echo_prefix $star_align --readFilesIn $unaligned_tagged_filtered_fastq
-    files_to_delete="$files_to_delete $unaligned_tagged_filtered_fastq"
-
-    # Stage 3
-    $echo_prefix $qsort_aligned
-
-    # Stage 4
-    $echo_prefix $merge_bam O=$tmpdir/merged.bam
-    $echo_prefix $tag_with_gene_exon I=$tmpdir/merged.bam
-    files_to_delete="$files_to_delete $tmpdir/merged.bam"
-
-fi
+# Stage 4
+$echo_prefix $merge_bam O=$tmpdir/merged.bam
+$echo_prefix $tag_with_gene_exon I=$tmpdir/merged.bam
+files_to_delete="$files_to_delete $tmpdir/merged.bam"
 
 $echo_prefix rm $files_to_delete
