b3a3705c20b5313c41465a4705f9671985656f14
Dario Romagnoli <dario.romagnoli87@gmail.com>
2020-03-25 15:17:38 +0100

    Few changes to make_graphs.R (add version)

--- code/make_graphs.R
+++ code/make_graphs.R
@@ -1,4 +1,6 @@
 #!/usr/bin/Rscript
+# 2020-03-25
+# version 0.1
 suppressMessages(library(tidyverse))
 
 theme_set(theme_minimal())
@@ -6,12 +8,13 @@ theme_set(theme_minimal())
 argv <- commandArgs(trailingOnly=TRUE)
 
 if (length(argv) == 1) {
-  n_cells <- Inf
+    n_cells <- Inf
 } else if (length(argv) == 2){
-  n_cells <- as.integer(argv[2])
+    n_cells <- as.integer(argv[2])
 } else {
-  stop(call. = FALSE, paste0("Wrong number of arguments.\n\n",
-       "  Usage: make_graphs.R <summary-path> [max number of cells]\n\n"))
+    stop(call. = FALSE,
+         paste0("Wrong number of arguments.\n\n",
+                "  Usage: make_graphs.R <summary-path> [max number of cells]\n\n"))
 }
 
 flags <- c("LX", "L1", "L2", "I", "D", "J", "K", "B", "PASS")
@@ -21,15 +24,15 @@ fname1 <- paste0(argv[1], ".errors.csv")
 errors <- read_tsv(fname1, col_types=cols())
 errors <- mutate(errors, Error=factor(Error, rev(flags)))
 p1 <- ggplot(errors, aes(Fraction, Error, color=Error, label=round(Fraction, 2))) +
-  geom_point() +
-  geom_segment(aes(x=0, xend=Fraction, y=Error, yend=Error)) +
-  xlim(0, 1) +
-  labs(title="Errors in barcode identification") +
-  theme(axis.title.y=element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.grid.major = element_line(size=.25),
-        panel.grid.minor.y = element_blank(),
-        legend.position = "none")
+    geom_point() +
+    geom_segment(aes(x=0, xend=Fraction, y=Error, yend=Error)) +
+    xlim(0, 1) +
+    labs(title="Errors in barcode identification") +
+    theme(axis.title.y=element_blank(),
+          panel.grid.minor = element_blank(),
+          panel.grid.major = element_line(size=.25),
+          panel.grid.minor.y = element_blank(),
+          legend.position = "none")
 
 fname2 <- paste0(argv[1], ".cell_barcodes.csv")
 barcodes <- read_tsv(fname2, col_type = "cid") %>%
@@ -41,30 +44,28 @@ barcodes_df <- bind_rows(barcodes) %>%
   mutate(CumFrac = CumSum/max(CumSum))
 
 p2 <- ggplot(barcodes_df, aes(Rank, Count)) +
-  geom_line() +
-  labs(title="Absolute Read Count", x="Ranked Cells", y=expression("Read Count Per Cell")) +
-  scale_x_continuous(
-                     labels=function(x) trimws(format(x, big.mark = ","))) +
-  scale_y_continuous(trans="log10",
-                     labels=function(x) trimws(format(x, big.mark = ","))) +
-  theme(
-        axis.text.y = element_text(angle=90, hjust=.5),
-        panel.grid.major = element_line(size=.25),
-        panel.grid.minor = element_blank(),
-        legend.position = "none")
+    geom_line() +
+    labs(title="Absolute Read Count", x="Ranked Cells", y=expression("Read Count Per Cell")) +
+    scale_x_continuous(labels=function(x) trimws(format(x, big.mark = ","))) +
+    scale_y_continuous(trans="log10",
+                       labels=function(x) trimws(format(x, big.mark = ","))) +
+    theme(axis.text.y = element_text(angle=90, hjust=.5),
+          panel.grid.major = element_line(size=.25),
+          panel.grid.minor = element_blank(),
+          legend.position = "none")
 
 p3 <- ggplot(barcodes_df, aes(Rank, CumFrac)) +
-  geom_line()+
-  labs(title="Cumulative Fraction", x="Ranked Cells", y="Cumulative Fraction of Reads per Cell") +
-  scale_x_continuous(
-                     labels=function(x) trimws(format(x, big.mark = ","))) +
-  theme(
-        panel.grid.major = element_line(size=.25),
-        panel.grid.minor = element_blank(),
-        legend.position = "none")
+    geom_line()+
+    labs(title="Cumulative Fraction", x="Ranked Cells", y="Cumulative Fraction of Reads per Cell") +
+    scale_x_continuous(labels=function(x) trimws(format(x, big.mark = ","))) +
+    theme(panel.grid.major = element_line(size=.25),
+          panel.grid.minor = element_blank(),
+          legend.position = "none")
 
-pdf(paste0(argv[1], ".pdf"), width = 4*1.61803, height = 4)
+outfile <- paste0(argv[1], ".pdf")
+pdf(outfile, width = 4*1.61803, height = 4)
 print(p1)
 print(p2)
 print(p3)
-dev.off()
+graphics.off()
+message("PDF produced: ", outfile)
